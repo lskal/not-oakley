4b9a4f2af29cc46831b7bb4fc054487aa3ec7cb3
massimiliano arosio <massimiliano.arosio@abstract.it>
2026-02-25 16:10:40 +0100

    implemented 404 page

--- app/error.vue
+++ app/error.vue
@@ -1,12 +1,40 @@
 <script setup lang="ts">
 import type { NuxtError } from "#app";
+import CustomCta from "./components/buttons/CustomCta.vue";
+import AppFooter from "./components/footer/AppFooter.vue";
+import AppHeader from "./components/header/AppHeader.vue";
 
 const props = defineProps<{ error: NuxtError }>();
 </script>
 
 <template>
-  <div>
-    <h1>{{ error.status }}</h1>
-    <NuxtLink to="/">Go back home</NuxtLink>
+  <AppHeader />
+  <div class="errorWrapper">
+    <div class="container">
+      <h1>ADVENTURE AWAITS ELSEWHERE</h1>
+      <p>We couldn't find the page you were looking for</p>
+      <CustomCta
+        textValue="Return to the home page →"
+        link="/"
+        isUppercase
+        margin="40px 0 0 "
+      />
+    </div>
   </div>
+  <AppFooter />
 </template>
+
+<style scoped lang="scss">
+.errorWrapper {
+  height: 50dvh;
+  background: url("/images/imageHDTest4.jpg") top / cover no-repeat;
+
+  .container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 100%;
+  }
+}
+</style>
