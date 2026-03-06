0089f5a1b716c8ec763599932138bd387c84ba22
massimiliano arosio <massimiliano.arosio@abstract.it>
2026-03-06 12:14:30 +0100

    installed @vercel/speed-insights

--- app/layouts/categoryLayout.vue
+++ app/layouts/categoryLayout.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { SpeedInsights } from "@vercel/speed-insights/vue";
 import AppFooter from "../components/footer/AppFooter.vue";
 import AppHeader from "../components/header/AppHeader.vue";
 import SkipToMainContent from "../components/header/SkipToMainContent.vue";
@@ -13,6 +14,7 @@ import SkipToMainContent from "../components/header/SkipToMainContent.vue";
     </main>
     <AppFooter />
   </div>
+  <SpeedInsights />
 </template>
 
 <style scoped lang="scss">
